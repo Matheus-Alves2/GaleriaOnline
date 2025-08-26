dc2601aaa72da39dfb519f97f227121aeca206de
Matheus Alves <Matheus.procopio@aluno.senai.br>
2025-08-26 15:19:26 -0300

    :sparkles: feat: subindo o site  completo do galeria online tudo fucionando

--- GaleriaOnline.WebApi/obj/Debug/net8.0/GaleriaOnline.WebApi.AssemblyInfo.cs
+++ GaleriaOnline.WebApi/obj/Debug/net8.0/GaleriaOnline.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14671e95220b6bb0116a9e2ed529490334992a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
